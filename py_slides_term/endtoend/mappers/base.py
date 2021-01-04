986030d097f7dba86a518362552c00e5179e80fa
kumachan-mis <y.s11nov@fuji.waseda.jp>
2021-01-05 02:48:26 +0900

    refactor: split mappers

---
+++ py_slides_term/endtoend/mappers/base.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+from abc import ABCMeta, abstractmethod
+from typing import Dict, Union, Generic, TypeVar
+
+MappedValue = TypeVar("MappedValue")
+
+
+class BaseMapper(Generic[MappedValue], metaclass=ABCMeta):
+    def __init__(self):
+        self._map: Dict[str, MappedValue] = dict()
+
+    def add(self, name: str, value: MappedValue):
+        self._map[name] = value
+
+    def find(self, name: str) -> Union[MappedValue, None]:
+        return self._map.get(name)
+
+    @classmethod
+    @abstractmethod
+    def default_mapper(cls) -> BaseMapper[MappedValue]:
+        raise NotImplementedError(f"{cls.__name__}.default_mapper()")
