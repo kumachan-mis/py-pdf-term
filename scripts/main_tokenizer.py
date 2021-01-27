13d6d955b6c8f3612ae4ab5b9a3781358982fc83
kumachan-mis <y.s11nov@fuji.waseda.jp>
2021-01-27 17:57:24 +0900

    fix

--- scripts/main_tokenizer.py
+++ scripts/main_tokenizer.py
@@ -3,12 +3,10 @@ from argparse import ArgumentParser
 from py_slides_term.morphemes import SpaCyTokenizer
 
 if __name__ == "__main__":
-    from dataclasses import asdict
-
     parser = ArgumentParser()
     parser.add_argument("text", help="text to pass SpaCy tokenizer", type=str)
     args = parser.parse_args()
 
     results = SpaCyTokenizer().tokenize(args.text)
     for result in results:
-        print(asdict(result))
+        print(result.surface_form, result.pos, result.category, result.subcategory)
