3feea2f35b298ee935a8b7466020b4402c0b4107
kumachan-mis <y.s11nov@fuji.waseda.jp>
2021-01-29 17:10:19 +0900

    implement SymbolNameSplitter

--- py_slides_term/candidates/splitters/symname.py
+++ py_slides_term/candidates/splitters/symname.py
@@ -1,3 +1,4 @@
+import re
 from typing import List
 
 from .base import BaseSplitter
@@ -11,4 +12,28 @@ class SymbolNameSplitter(BaseSplitter):
         self._filter = candidate_filter
 
     def split(self, term: Term) -> List[Term]:
-        return [term]
+        num_morphemes = len(term.morphemes)
+        if num_morphemes < 2:
+            return [term]
+
+        last_str = str(term.morphemes[len(term.morphemes) - 1])
+        second_last_str = str(term.morphemes[len(term.morphemes) - 2])
+        last_may_symbol = re.fullmatch(r"[A-Za-z]", last_str)
+        second_last_may_symbol = re.fullmatch(r"[A-Za-z\-]", second_last_str)
+
+        if not last_may_symbol or second_last_may_symbol:
+            return [term]
+
+        non_symbol_morphemes = term.morphemes[: num_morphemes - 1]
+        symbol_morphemes = [term.morphemes[num_morphemes - 1]]
+        non_symbol_term = Term(non_symbol_morphemes, term.fontsize, term.augmented)
+        symbol_term = Term(symbol_morphemes, term.fontsize, term.augmented)
+
+        if not self._filter.is_candidate(non_symbol_term):
+            return [term]
+
+        splitted_terms = [non_symbol_term]
+        if self._filter.is_candidate(symbol_term):
+            splitted_terms.append(symbol_term)
+
+        return splitted_terms
