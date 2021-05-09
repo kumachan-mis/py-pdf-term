6bb0cc4dfc335bc5f690fe9f42b0cae09621882b
kumachan-mis <y.s11nov@fuji.waseda.jp>
2021-05-09 23:04:48 +0900

    fix: fix bugs

--- py_slides_term/endtoend/configs/method.py
+++ py_slides_term/endtoend/configs/method.py
@@ -7,7 +7,7 @@ from .base import BaseLayerConfig
 @dataclass(frozen=True)
 class MethodLayerConfig(BaseLayerConfig):
     method_type: Literal["single", "multi"] = "single"
-    method: str = "py_slides_term.methods.FLRHMethod"
+    method: str = "py_slides_term.FLRHMethod"
     hyper_params: Dict[str, Any] = field(default_factory=dict)
     ranking_cache: str = "py_slides_term.MethodLayerRankingFileCache"
     data_cache: str = "py_slides_term.MethodLayerRankingFileCache"
