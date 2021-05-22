08caf4bdd265924f10280bd905c3aef795bcbf23
kumachan-mis <y.s11nov@fuji.waseda.jp>
2021-05-22 18:16:44 +0900

    split file

---
+++ tests/candidates/test_spacing.py
@@ -0,0 +1,120 @@
+import pytest
+
+from py_slides_term.candidates import CandidateTermExtractor
+
+
+@pytest.fixture
+def extractor() -> CandidateTermExtractor:
+    return CandidateTermExtractor()
+
+
+def test_meaningful_space(extractor: CandidateTermExtractor):
+    candidates = extractor.extract_from_text("プログラミング言語 型体系")
+    assert len(candidates) == 2
+
+    candidate = candidates[0]
+    assert candidate.lang == "ja"
+    assert str(candidate) == "プログラミング言語"
+
+    candidate = candidates[1]
+    assert candidate.lang == "ja"
+    assert str(candidate) == "型体系"
+
+
+def test_meaningless_space(extractor: CandidateTermExtractor):
+    candidates = extractor.extract_from_text("情報通信ネットワ ーク")
+    assert len(candidates) == 1
+
+    candidate = candidates[0]
+    assert candidate.lang == "ja"
+    assert str(candidate) == "情報通信ネットワーク"
+
+
+def test_meaningful_space_with_space_post_english(extractor: CandidateTermExtractor):
+    candidates = extractor.extract_from_text("型体系 プログラミング言語 Programming Language")
+    assert len(candidates) == 2
+
+    candidate = candidates[1]
+    assert candidate.lang == "ja"
+    assert str(candidate) == "型体系"
+
+    candidate = candidates[0]
+    assert candidate.lang == "ja"
+    assert str(candidate) == "プログラミング言語Programming Language"
+
+
+def test_meaningful_space_with_nospace_post_english(extractor: CandidateTermExtractor):
+    candidates = extractor.extract_from_text("型体系 プログラミング言語Programming Language")
+    assert len(candidates) == 2
+
+    candidate = candidates[0]
+    assert candidate.lang == "ja"
+    assert str(candidate) == "型体系"
+
+    candidate = candidates[1]
+    assert candidate.lang == "ja"
+    assert str(candidate) == "プログラミング言語Programming Language"
+
+
+def test_meaningful_space_with_space_pre_english(extractor: CandidateTermExtractor):
+    candidates = extractor.extract_from_text("Programming Language プログラミング言語 型体系")
+    assert len(candidates) == 2
+
+    candidate = candidates[0]
+    assert candidate.lang == "ja"
+    assert str(candidate) == "Programming Languageプログラミング言語"
+
+    candidate = candidates[1]
+    assert candidate.lang == "ja"
+    assert str(candidate) == "型体系"
+
+
+def test_meaningful_space_with_nospace_pre_english(extractor: CandidateTermExtractor):
+    candidates = extractor.extract_from_text("Programming Languageプログラミング言語 型体系")
+    assert len(candidates) == 2
+
+    candidate = candidates[0]
+    assert candidate.lang == "ja"
+    assert str(candidate) == "Programming Languageプログラミング言語"
+
+    candidate = candidates[1]
+    assert candidate.lang == "ja"
+    assert str(candidate) == "型体系"
+
+
+def test_meaningless_space_with_space_post_english(extractor: CandidateTermExtractor):
+    candidates = extractor.extract_from_text("情報通信ネ ットワー ク OSI reference model")
+    assert len(candidates) == 1
+
+    candidate = candidates[0]
+    assert candidate.lang == "ja"
+    assert str(candidate) == "情報通信ネットワークOSI reference model"
+
+
+def test_meaningless_space_with_nospace_post_english(extractor: CandidateTermExtractor):
+    candidates = extractor.extract_from_text("情報通信ネ ットワー クOSI reference model")
+    assert len(candidates) == 1
+
+    candidate = candidates[0]
+    assert candidate.lang == "ja"
+    assert str(candidate) == "情報通信ネットワークOSI reference model"
+
+
+def test_meaningless_space_with_space_pre_english(extractor: CandidateTermExtractor):
+    candidates = extractor.extract_from_text("computer network 情報通信ネ ットワー ク")
+
+    assert len(candidates) == 1
+
+    candidate = candidates[0]
+    assert candidate.lang == "ja"
+    assert str(candidate) == "computer network情報通信ネットワーク"
+
+
+def test_meaningless_space_with_nospace_pre_english(extractor: CandidateTermExtractor):
+    candidates = extractor.extract_from_text("computer network情報通信ネ ットワー ク")
+
+    assert len(candidates) == 1
+
+    candidate = candidates[0]
+    assert candidate.lang == "ja"
+    assert str(candidate) == "computer network情報通信ネットワーク"
