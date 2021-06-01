b028527dd41def3239b3c31abdd59aeacfbe9099
kumachan-mis <y.s11nov@fuji.waseda.jp>
2021-06-01 12:06:38 +0900

    feat: filter out indexed phonetic term

--- py_slides_term/endtoend/_endtoend/caches/consts.py
+++ py_slides_term/endtoend/_endtoend/caches/consts.py
@@ -1,5 +1,11 @@
 import os
 
 DEFAULT_CACHE_DIR = os.path.abspath(
-    os.path.join(os.path.dirname(__file__), "..", "..", "__py_slides_term_cache__")
+    os.path.join(
+        os.path.dirname(__file__),
+        "..",
+        "..",
+        "..",
+        "__py_slides_term_cache__",
+    )
 )
