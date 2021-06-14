5a921c6b4e8e0fdab77a4ccfe92657ebce134d18
kumachan-mis <y.s11nov@fuji.waseda.jp>
2021-06-14 22:15:04 +0900

    fix

--- py_slides_term/tokenizer/langs.py
+++ py_slides_term/tokenizer/langs.py
@@ -1,13 +1,2 @@
-from ._tokenizer.langs import BaseLanguageTokenizer, JapaneseTokenizer, EnglishTokenizer
-from ._tokenizer.classifiers import (
-    JapaneseMorphemeClassifier,
-    EnglishMorphemeClassifier,
-)
-
-__all__ = [
-    "BaseLanguageTokenizer",
-    "JapaneseTokenizer",
-    "EnglishTokenizer",
-    "JapaneseMorphemeClassifier",
-    "EnglishMorphemeClassifier",
-]
+from ._tokenizer.langs import *  # NoQA
+from ._tokenizer.classifiers import *  # NoQA
