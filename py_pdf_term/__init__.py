9c40c8ad56406946352392f5a3446b2df27b824c
kumachan-mis <y.s11nov@fuji.waseda.jp>
2022-03-09 17:29:08 +0900

    v0.18.0

--- py_pdf_term/__init__.py
+++ py_pdf_term/__init__.py
@@ -1,6 +1,6 @@
 from .endtoend import DomainPDFList, PDFTechnicalTermList, PyPDFTermExtractor
 
-__version__ = "0.17.1"
+__version__ = "0.18.0"
 
 # isort: unique-list
 __all__ = ["DomainPDFList", "PDFTechnicalTermList", "PyPDFTermExtractor"]
