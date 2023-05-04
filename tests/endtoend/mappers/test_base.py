8994af85a2b3d78477ef1b80dcfe1e07bc2d984b
kumachan-mis <29433058+kumachan-mis@users.noreply.github.com>
2023-05-04 13:30:54 +0900

    reduce test cases

---
+++ tests/endtoend/mappers/test_base.py
@@ -0,0 +1,96 @@
+from abc import ABCMeta
+from typing import Type
+from pytest import raises
+
+from py_pdf_term.endtoend._endtoend.mappers import BaseMapper
+
+
+class BaseTestMappedValue(metaclass=ABCMeta):
+    __test__ = False
+
+    def __init__(self, value: str) -> None:
+        self.value = value
+
+
+class TestDefaultMappedValue(BaseTestMappedValue):
+    __test__ = True
+
+    def __init__(self) -> None:
+        super().__init__("Default")
+
+
+class TestUserDefinedMappedValue(BaseTestMappedValue):
+    __test__ = True
+
+    def __init__(self) -> None:
+        super().__init__("UserDefined")
+
+
+class TestMapper(BaseMapper[Type[BaseTestMappedValue]]):
+    @classmethod
+    def default_mapper(cls) -> BaseMapper[Type[BaseTestMappedValue]]:
+        mapper = cls()
+        mapper.add("py_pdf_term.TestDefaultMappedValue", TestDefaultMappedValue)
+        return mapper
+
+
+def test_find():
+    mapper = TestMapper.default_mapper()
+
+    cls = mapper.find("py_pdf_term.TestDefaultMappedValue")
+    assert cls == TestDefaultMappedValue
+
+    raises(KeyError, lambda: mapper.find("py_pdf_term.TestUnknownMappedValue"))
+
+
+def test_find_or_none():
+    mapper = TestMapper.default_mapper()
+
+    cls = mapper.find_or_none("py_pdf_term.TestDefaultMappedValue")
+    assert cls == TestDefaultMappedValue
+
+    cls = mapper.find_or_none("py_pdf_term.TestUnknownMappedValue")
+    assert cls is None
+
+
+def test_bulk_find():
+    mapper = TestMapper.default_mapper()
+
+    clses = mapper.bulk_find(["py_pdf_term.TestDefaultMappedValue"])
+    assert clses == [TestDefaultMappedValue]
+
+    raises(KeyError, lambda: mapper.bulk_find(["py_pdf_term.TestUnknownMappedValue"]))
+
+
+def test_bulk_find_or_none():
+    mapper = TestMapper.default_mapper()
+
+    clses = mapper.bulk_find_or_none(
+        [
+            "py_pdf_term.TestDefaultMappedValue",
+            "py_pdf_term.TestUserDefinedMappedValue",
+            "py_pdf_term.TestUnknownMappedValue",
+        ]
+    )
+    assert clses == [TestDefaultMappedValue, None, None]
+
+
+def test_add_then_remove():
+    mapper = TestMapper.default_mapper()
+
+    cls = mapper.find("py_pdf_term.TestDefaultMappedValue")
+    assert cls == TestDefaultMappedValue
+    raises(KeyError, lambda: mapper.find("py_pdf_term.TestUserDefinedMappedValue"))
+
+    mapper.add("py_pdf_term.TestUserDefinedMappedValue", TestUserDefinedMappedValue)
+
+    cls = mapper.find("py_pdf_term.TestDefaultMappedValue")
+    assert cls == TestDefaultMappedValue
+    cls = mapper.find("py_pdf_term.TestUserDefinedMappedValue")
+    assert cls == TestUserDefinedMappedValue
+
+    mapper.remove("py_pdf_term.TestUserDefinedMappedValue")
+
+    cls = mapper.find("py_pdf_term.TestDefaultMappedValue")
+    assert cls == TestDefaultMappedValue
+    raises(KeyError, lambda: mapper.find("py_pdf_term.TestUserDefinedMappedValue"))
---
+++ tests/endtoend/mappers/test_cache.py
@@ -0,0 +1,86 @@
+from typing import Any
+
+from py_pdf_term.mappers import (
+    CandidateLayerCacheMapper,
+    XMLLayerCacheMapper,
+    MethodLayerDataCacheMapper,
+    MethodLayerRankingCacheMapper,
+    StylingLayerCacheMapper,
+)
+from py_pdf_term.endtoend.caches import (
+    CandidateLayerNoCache,
+    CandidateLayerFileCache,
+    XMLLayerNoCache,
+    XMLLayerFileCache,
+    MethodLayerDataNoCache,
+    MethodLayerDataFileCache,
+    MethodLayerRankingNoCache,
+    MethodLayerRankingFileCache,
+    StylingLayerNoCache,
+    StylingLayerFileCache,
+)
+
+
+def test_xml_layer_cache_default_mapper():
+    mapper = XMLLayerCacheMapper.default_mapper()
+
+    clses = mapper.bulk_find_or_none(
+        [
+            "py_pdf_term.XMLLayerNoCache",
+            "py_pdf_term.XMLLayerFileCache",
+            "py_pdf_term.XMLLayerUnknownCache",
+        ]
+    )
+    assert clses == [XMLLayerNoCache, XMLLayerFileCache, None]
+
+
+def test_candidate_layer_cache_default_mapper():
+    mapper = CandidateLayerCacheMapper.default_mapper()
+
+    clses = mapper.bulk_find_or_none(
+        [
+            "py_pdf_term.CandidateLayerNoCache",
+            "py_pdf_term.CandidateLayerFileCache",
+            "py_pdf_term.CandidateLayerUnknownCache",
+        ]
+    )
+    assert clses == [CandidateLayerNoCache, CandidateLayerFileCache, None]
+
+
+def test_method_layer_data_cache_default_mapper():
+    mapper = MethodLayerDataCacheMapper.default_mapper()
+
+    clses = mapper.bulk_find_or_none(
+        [
+            "py_pdf_term.MethodLayerDataNoCache",
+            "py_pdf_term.MethodLayerDataFileCache",
+            "py_pdf_term.MethodLayerDataUnknownCache",
+        ]
+    )
+    assert clses == [MethodLayerDataNoCache[Any], MethodLayerDataFileCache[Any], None]
+
+
+def test_method_layer_ranking_cache_default_mapper():
+    mapper = MethodLayerRankingCacheMapper.default_mapper()
+
+    clses = mapper.bulk_find_or_none(
+        [
+            "py_pdf_term.MethodLayerRankingNoCache",
+            "py_pdf_term.MethodLayerRankingFileCache",
+            "py_pdf_term.MethodLayerRankingUnknownCache",
+        ]
+    )
+    assert clses == [MethodLayerRankingNoCache, MethodLayerRankingFileCache, None]
+
+
+def test_styling_layer_cache_default_mapper():
+    mapper = StylingLayerCacheMapper.default_mapper()
+
+    clses = mapper.bulk_find_or_none(
+        [
+            "py_pdf_term.StylingLayerNoCache",
+            "py_pdf_term.StylingLayerFileCache",
+            "py_pdf_term.StylingLayerUnknownCache",
+        ]
+    )
+    assert clses == [StylingLayerNoCache, StylingLayerFileCache, None]
